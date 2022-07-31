ad2559a598957259bafc02808aa0a00898f9b314
cepjeff <jafar@siklus.id>
2022-07-31 10:16:02 +0700

    s

--- danamas/danamas/doctype/aplikasi/aplikasi.py
+++ danamas/danamas/doctype/aplikasi/aplikasi.py
@@ -6,7 +6,7 @@ import math
 
 import frappe
 from frappe import _
-from frappe.utils import add_months, flt, get_last_day, getdate, now_datetime
+from frappe.utils import add_months, add_days,flt, get_last_day, getdate, now_datetime,add_to_date
 
 from frappe.model.document import Document
 
@@ -28,21 +28,62 @@ class Aplikasi(Document):
 
 				plafon = flt(plafon - angsuran)
 				if plafon < 0:
-					#angsuran += plafon
 					plafon = 0.0
+
+				if self.periode_angsuran == 'Bulanan':
+					self.append(
+						"list_angsuran_pinjaman",
+						{
+							"tanggal_tagihan": add_single_month(payment_date),
+							"pokok": pokok_angsuran,
+							"bunga": bunga_angsuran,
+							"angsuran": angsuran,
+							"saldo_sisa": plafon,
+						},
+					)
+					next_payment_date = add_single_month(payment_date)
+					payment_date = next_payment_date
 				
-				self.append(
-					"list_angsuran_pinjaman",
-					{
-						"tanggal_tagihan": add_single_month(payment_date),
-						"pokok": pokok_angsuran,
-						"bunga": bunga_angsuran,
-						"angsuran": angsuran,
-						"saldo_sisa": plafon,
-					},
-				)
-				next_payment_date = add_single_month(payment_date)
-				payment_date = next_payment_date
+				if self.periode_angsuran == 'Mingguan Per 10 Hari':
+					self.append(
+						"list_angsuran_pinjaman",
+						{
+							"tanggal_tagihan": add_single_week_10(payment_date),
+							"pokok": pokok_angsuran,
+							"bunga": bunga_angsuran,
+							"angsuran": angsuran,
+							"saldo_sisa": plafon,
+						},
+					)
+					next_date = add_single_week_10(payment_date)
+					payment_date = next_date
+				if self.periode_angsuran == 'Mingguan Per 7 Hari':
+					self.append(
+						"list_angsuran_pinjaman",
+						{
+							"tanggal_tagihan": add_single_week_7(payment_date),
+							"pokok": pokok_angsuran,
+							"bunga": bunga_angsuran,
+							"angsuran": angsuran,
+							"saldo_sisa": plafon,
+						},
+					)
+					next_date = add_single_week_7(payment_date)
+					payment_date = next_date
+					
+				if self.periode_angsuran == 'Harian':
+					self.append(
+						"list_angsuran_pinjaman",
+						{
+							"tanggal_tagihan": add_single_days(payment_date),
+							"pokok": pokok_angsuran,
+							"bunga": bunga_angsuran,
+							"angsuran": angsuran,
+							"saldo_sisa": plafon,
+						},
+					)
+					next_date = add_single_days(payment_date)
+					payment_date = next_date
 				i += 1
 	
 def add_single_month(date):
@@ -50,3 +91,21 @@ def add_single_month(date):
 		return get_last_day(add_months(date, 1))
 	else:
 		return add_months(date, 1)
+
+def add_single_week_10(date):
+	if getdate(date) == get_last_day(date):
+		return add_days(date, 10)
+	else:
+		return add_days(date, 10)
+
+def add_single_week_7(date):
+	if getdate(date) == get_last_day(date):
+		return add_days(date, 7)
+	else:
+		return add_days(date, 7)
+
+def add_single_days(date):
+	if getdate(date) == get_last_day(date):
+		return add_days(date, 1)
+	else:
+		return add_days(date, 1)
