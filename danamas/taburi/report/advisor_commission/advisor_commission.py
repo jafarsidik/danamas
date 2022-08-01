c9ca65ba455689343d5ddf27363b26831d90fe23
cepjeff <jafar@siklus.id>
2022-08-01 17:01:25 +0700

    taburi

--- danamas/taburi/report/advisor_commission/advisor_commission.py
+++ danamas/taburi/report/advisor_commission/advisor_commission.py
@@ -15,41 +15,23 @@ def execute(filters=None):
 		"options": "Advisor"
 	},
 	{
-		"fieldname": "debit",
-		"label": "Debit",
+		"fieldname": "total",
+		"label": "Total",
 		"fieldtype": "Currency"
 	},
-	{ "fieldname": "januari", "label": "Januari", "fieldtype": "Currency" },
-	{ "fieldname": "februari","label": "Februari","fieldtype": "Currency"},
-	{ "fieldname": "maret","label": "Maret","fieldtype": "Currency"},
-	{ "fieldname": "april","label": "April","fieldtype": "Currency"},
-	{ "fieldname": "mei","label": "Mei","fieldtype": "Currency"},
-	{ "fieldname": "juni","label": "Juni","fieldtype": "Currency"},
-	{ "fieldname": "juli","label": "Juli","fieldtype": "Currency"},
-	{ "fieldname": "agustus","label": "Agustus","fieldtype": "Currency"},
-	{ "fieldname": "september","label": "September","fieldtype": "Currency"},
-	{ "fieldname": "oktober","label": "Oktober","fieldtype": "Currency"},
-	{ "fieldname": "november","label": "November","fieldtype": "Currency"},
-	{ "fieldname": "desember","label": "Desember","fieldtype": "Currency"}
+	{
+		"fieldname": "komission_fee",
+		"label": "Komision Fee",
+		"fieldtype": "Currency"
+	}
 	]
 	data = frappe.db.sql(
 		f"""
 		SELECT
 			c.advisor,
 			month(t.transaction_date),
-			sum(t.debit) as debit,
-			sum(case when (month(t.transaction_date) ='01') then (t.debit*1/100) else 0 end) as januari,
-			sum(case when (month(t.transaction_date) ='02') then (t.debit*1/100) else 0 end) as februari,
-			sum(case when (month(t.transaction_date) ='03') then (t.debit*1/100) else 0 end) as maret,
-			sum(case when (month(t.transaction_date) ='04') then (t.debit*1/100) else 0 end) as april,
-			sum(case when (month(t.transaction_date) ='05') then (t.debit*1/100) else 0 end) as mei,
-			sum(case when (month(t.transaction_date) ='06') then (t.debit*1/100) else 0 end) as juni,
-			sum(case when (month(t.transaction_date) ='07') then (t.debit*1/100) else 0 end) as juli,
-			sum(case when (month(t.transaction_date) ='08') then (t.debit*1/100) else 0 end) as agustus,
-			sum(case when (month(t.transaction_date) ='09') then (t.debit*1/100) else 0 end) as september,
-			sum(case when (month(t.transaction_date) ='10') then (t.debit*1/100) else 0 end) as oktober,
-			sum(case when (month(t.transaction_date) ='11') then (t.debit*1/100) else 0 end) as november,
-			sum(case when (month(t.transaction_date) ='12') then (t.debit*1/100) else 0 end) as desember
+			sum(t.debit - t.credit) as total,
+			sum((t.debit-t.credit)*1/100) as komission_fee
 		FROM `tabTransaction` AS t
 		left join `tabCustomers` as c on `c`.`name` = `t`.`customers`
 		where year(transaction_date) = year(curdate())
