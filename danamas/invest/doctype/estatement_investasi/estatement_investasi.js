6c945dfe198e66ef60bbd661613581cb32aa137f
cepjeff <jafar@siklus.id>
2022-10-20 07:30:55 +0700

    g

--- danamas/invest/doctype/estatement_investasi/estatement_investasi.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2022, mjs and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Estatement Investasi', {
-	// refresh: function(frm) {
-
-	// }
-});
